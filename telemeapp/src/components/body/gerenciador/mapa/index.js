87d6836bea67ea22f67665e45b4d8e10aee3d160
andreocunha <andreoliveiracunha20@gmail.com>
2021-04-28 16:29:32 -0300

    Adição do mapa

--- telemeapp/src/components/body/gerenciador/mapa/index.js
+++ telemeapp/src/components/body/gerenciador/mapa/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './style.css'
+import { TileLayer, MapContainer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-const Mapa = () => (
-    <div className ="mapa">
-        Mapa
-    </div>
-)
+const Mapa = () => {
 
+    return(
+        <div className ="container">
+            <MapContainer center={[-20.2769499, -40.3068654]} zoom={15} scrollWheelZoom={false} style={{width: '100%', height: '100%' }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            </MapContainer>
+        </div>
+    )
+}
+    
 export default Mapa
