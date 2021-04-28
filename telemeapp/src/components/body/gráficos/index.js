dae55a51a84b664573a6bdb35154c134ce2cc7e4
Guilherme Goes Zanetti <guigoes8@hotmail.com>
2021-04-28 20:48:00 -0300

    Geração de valores aleatórios no gráfico

--- telemeapp/src/components/body/gráficos/index.js
+++ telemeapp/src/components/body/gráficos/index.js
@@ -8,40 +8,60 @@ class Graficos extends Component {
     constructor() {
 		super();
 		this.generateDataPoints = this.generateDataPoints.bind(this);
+        this.state = {
+            data: []
+        }
 	}
 	
 	generateDataPoints(noOfDps) {
-		var xVal = 1, yVal = 100;
+		var xVal = noOfDps, yVal = 100;
 		var dps = [];
 		for(var i = 0; i < noOfDps; i++) {
 			yVal = yVal +  Math.round(5 + Math.random() *(-6-6));
 			dps.push({tempo: xVal, Corrente: yVal});	
-			xVal++;
+			xVal--;
 		}
 		return dps;
 	}
 	
+    componentDidMount(){
+        this.setState({data: this.generateDataPoints(500)});
+        setInterval(() => { 
+            var novoData = this.state.data;
+            novoData.shift();
+            novoData.push({tempo: 0, Corrente: this.state.data[this.state.data.length-1].Corrente + 5 + Math.random()*(-10)});
+
+            novoData.forEach((value, index) => {
+                value.tempo = index;
+            })
+            this.setState({data: novoData});
+
+            //Vai aumentando o número de pontos exibidos
+            //this.setState({data: [...this.state.data,  {tempo: this.state.data[this.state.data.length-1].tempo+1 , Corrente: Math.random()*100}]});
+            //console.log(this.state.data); 
+        }, 500);
+    }
+
 	render() {
-        var data = this.generateDataPoints(50);
 
 		return (
         
         <div className= "grafico">
             <ResponsiveContainer width="100%" height="80%">
                 <LineChart
-                    data={data}
+                    data={this.state.data}
                     margin={{ top: 35, right: 30, left: 10, bottom: 30 }}
                     >
                     <CartesianGrid  verticalFill={['rgba(0, 0, 0, 0.4)', 'rgba(0, 0, 0, 0.6)']} horizontalFill={['#ccc', '#fff']} />
                     <Tooltip contentStyle={{backgroundColor:'rgba(0, 0, 0, 0.6)'}}/>
-                    <XAxis dataKey="tempo" stroke='white'>
+                    <XAxis dataKey="tempo" stroke='white' minTickGap={50} interval="preserveStartEnd">
                         <Label value="Tempo" stroke='white' offset={-20} position="insideBottomRight" />
                     </XAxis>
                     <YAxis stroke='white'>
                         <Label value="Corrente" stroke='white' offset={20} position="top" />
                     </YAxis>
                     <Legend/>
-                    <Line type='monotone' dataKey='Corrente' stroke='white' />
+                    <Line type='monotone' dataKey='Corrente' stroke='white' dot={false} />
                 </LineChart>
             </ResponsiveContainer>
             
