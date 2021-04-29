5ac796564f0c3d48f89e9e922558a5f94f97cd0b
estevaons <estevao_gt@hotmail.com>
2021-04-29 16:22:51 -0300

    estilizações

--- telemeapp/src/components/header/index.js
+++ telemeapp/src/components/header/index.js
@@ -16,9 +16,17 @@ const Header = ()=> (
         <p id="poente--name">POENTE</p>
         <img src= {Logo} width ="50px" height="50px" id="logo" alt=""/>
         <p id="voltas--name">VOLTAS</p>
-        <Voltas id="voltas--componente"/>
+
+        <div className="voltas--componente-container">
+            <Voltas id="voltas--componente"/>
+        </div>
+
         <p id="tempo--name">TEMPO</p>
-        <Tempo id="tempo--componente"/>
+
+        <div className="tempo--componente--container">
+            <Tempo id="tempo--componente"/>
+        </div>
+
         <img src={Bandeira} width ="70px" height="50px" id="bandeira" alt=""/>
 
     </div>
