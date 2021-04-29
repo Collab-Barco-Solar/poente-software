6c9b49b11ff8957d9eec9a4f0e6d088be800ea32
estevaons <estevao_gt@hotmail.com>
2021-04-29 02:50:20 -0300

    displays de acionamentos

--- telemeapp/src/components/body/instantaneas/index.js
+++ telemeapp/src/components/body/instantaneas/index.js
@@ -3,6 +3,9 @@ import './style.css'
 
 import sendAsync from '../../../message-control/renderer';
 
+let onOff = 1;
+let dms = 0;
+
 class Instantaneas extends Component {
     constructor() {
         super();
@@ -33,29 +36,53 @@ class Instantaneas extends Component {
     render() {
         return(
             <div className= "instantaneas">
-                <div className="valores">
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].cBarramento ) || "Carregando"} A</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].tModulos ) || "Carregando"} V</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].cBaterias ) || "Carregando"} A</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].tBaterias ) || "Carregando"} V</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].cBateriasAux ) || "Carregando"} A</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].tBateriasAux ) || "Carregando"} V</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].pPotenciometro ) || "Carregando"}</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].velocidade ) || "Carregando"} nós</p>
-                    <p className="valores--itens">{( this.state.data && this.state.data[0].temperatura ) || "Carregando"} ºC</p>
+                <div className="instantaneas--superior">
+
+                    <div className="valores">
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].cBarramento ) || "Carregando"} A</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].tModulos ) || "Carregando"} V</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].cBaterias ) || "Carregando"} A</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].tBaterias ) || "Carregando"} V</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].cBateriasAux ) || "Carregando"} A</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].tBateriasAux ) || "Carregando"} V</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].pPotenciometro ) || "Carregando"}</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].velocidade ) || "Carregando"} nós</p>
+                        <p className="valores--itens">{( this.state.data && this.state.data[0].temperatura ) || "Carregando"} ºC</p>
+                    </div>
+                    <div className="nomes">
+                        <p className="nomes--itens">Corrente Motor</p>
+                        <p className="nomes--itens">Tensão Módulos</p>
+                        <p className="nomes--itens">Corrente Baterias</p>
+                        <p className="nomes--itens">Tensão Baterias</p>
+                        <p className="nomes--itens">Corrente Bateria Aux</p>
+                        <p className="nomes--itens">Tensão Bateria Aux</p>
+                        <p className="nomes--itens">Posição Potenciômetro</p>
+                        <p className="nomes--itens">Velocidade</p>
+                        <p className="nomes--itens">Temperatura</p>
+                    </div>          
                 </div>
-                <div className="nomes">
-                    <p className="nomes--itens">Corrente Motor</p>
-                    <p className="nomes--itens">Tensão Módulos</p>
-                    <p className="nomes--itens">Corrente Baterias</p>
-                    <p className="nomes--itens">Tensão Baterias</p>
-                    <p className="nomes--itens">Corrente Bateria Aux</p>
-                    <p className="nomes--itens">Tensão Bateria Aux</p>
-                    <p className="nomes--itens">Posição Potenciômetro</p>
-                    <p className="nomes--itens">Velocidade</p>
-                    <p className="nomes--itens">Temperatura</p>
-                </div>                
+                <div className="acionamentos">
+                    <div className="acionamentos--superior">
+                        <div className="on-off-display">
+                           {onOff === 1 ? <div className="on-off-ON"></div> : <div className="on-off-OFF"></div>}
+                            <p className="on-off-name">ON/OFF</p>
+                        </div>
+                        <div className="dms-display">
+                            {dms === 1 ? <div className="dms-ON"></div> : <div className="dms-OFF"></div>}
+                            <p className="dms-name">DMS</p>
+                        </div>
+                        <div>
+                            <div className="re"></div>
+                            <p>RÉ</p>
+                        </div>
+                    </div>
+                    <div className="acionamentos--inferior">
+                        <div>BOMBA BB</div>
+                        <div>BOMBA BE</div>
+                    </div>
+                </div>
             </div>
+            
         );
     }
 }
